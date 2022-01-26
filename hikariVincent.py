8d0fc0c63ada73291f69cb1860fac7f042ffabe7
Retr0_D3DSEC <47493043+aayura@users.noreply.github.com>
2022-01-26 14:24:01 +0530

    Testing

--- hikariVincent.py
+++ hikariVincent.py
@@ -11,7 +11,7 @@ bot = lightbulb.BotApp(token=my_secret, default_enabled_guilds = (73319874034873
 async def on_started(event):
   print("Bot is ready, go get them tiger!")
 
-@bot.listen(hikari.MessageCreateEvent)
+@bot.listen(hikari.GuildMessageCreateEvent)
 async def on_message(event):
   await event.respond(f"{event.author.mention} You typed a message!")
 
