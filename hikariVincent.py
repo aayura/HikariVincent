521f78236a5587184f8997a6a3a21cd0f73612b1
Retr0_D3DSEC <47493043+aayura@users.noreply.github.com>
2022-01-26 14:32:27 +0530

    test

--- hikariVincent.py
+++ hikariVincent.py
@@ -1,4 +1,3 @@
-from email import message
 import hikari
 import lightbulb
 import random
@@ -6,15 +5,19 @@ import os
 from webserver import keep_alive
 
 my_secret = os.environ['DISCORD_BOT_TOKEN']
-bot = lightbulb.BotApp(token=my_secret, default_enabled_guilds = (733198740348731442))
+bot = lightbulb.BotApp(
+    token=my_secret, default_enabled_guilds=(733198740348731442))
+
 
 @bot.listen(hikari.StartedEvent)
 async def on_started(event):
   print("Bot is ready, go get them tiger!")
 
+
 @bot.listen(hikari.MessageCreateEvent)
 async def on_message(event):
   await event.message.respond(f"{event.message.author.mention} You typed a message!")
+
 
 @bot.command
 @lightbulb.command('test', 'Testing commands.')
@@ -22,17 +25,20 @@ async def on_message(event):
 async def testing_commands(ctx):
   pass
 
+
 @testing_commands.child
 @lightbulb.command('ping', 'Says Pong!')
 @lightbulb.implements(lightbulb.SlashSubCommand)
 async def ping(ctx):
   await ctx.respond("Pong!")
 
+
 @bot.command
 @lightbulb.command('gamble', 'Gambling commands.')
 @lightbulb.implements(lightbulb.SlashCommandGroup)
 async def gambling_commands(ctx):
   pass
+
 
 @gambling_commands.child
 @lightbulb.command('toss', 'toss a coin!')
